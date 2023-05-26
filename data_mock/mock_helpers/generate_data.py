fb1b012ae0a7dc6610854a1a9914a272d1fccc66
Paul Tremblay <phtremblay@sbgtv.com>
2023-05-26 10:09:57 -0700

    cleaning up

--- data_mock/mock_helpers/generate_data.py
+++ data_mock/mock_helpers/generate_data.py
@@ -1,3 +1,5 @@
+import types
+
 from data_mock.google.cloud.bigquery import SchemaField
 from data_mock.mock_helpers.provider import Data
 
@@ -21,15 +23,31 @@ def generate_field(f:SchemaField) -> dict:
             current[2][current_name] = generate_value(field_type = current_o.field_type)
     return final
 
+def convert_dict_to_list(data):
+    #not used
+    new_l = []
+    stack = [{'data':data,  'append_to' : new_l}]
+    counter = 0
+    while len(stack) != 0:
+        stack_info = stack.pop()
+        stack_data = stack_info['data']
+        append_to = stack_info['append_to']
+        for n, i in enumerate(stack_data):
+            assert isinstance(i, dict)
+            for key in i.keys():
+                value = i[key]
+                if not isinstance(value, list):
+                    append_to.append((key, value))
+                else:
+                    append_to.append([key])
+                    stack.append({'data': value, 'append_to' : append_to[-1]})
+    return new_l
 
-
-def generate_data(schema, num_rows = 1):
+def generate_data(schema:list, num_rows:int = 1) -> types.GeneratorType:
     for i in range(num_rows):
-        temp = []
-        temp2 = []
+        l = []
         for j in schema:
             r = generate_field(j)
-            temp.append(r)
             for key in r.keys():
-                temp2.append(Data(name = key,value = r[key]))
-        yield temp2
+                l.append(Data(name = key,value = r[key]))
+        yield l
