da431180e8a33d7dfe11cee4f355ea1bcaa444e5
Henry <paulhtremblay@gmail.com>
2023-06-01 23:03:28 -0700

    fixng build

--- build.sh
+++ build.sh
@@ -1,4 +1,2 @@
-#gcloud builds submit --region=us-west1 --tag gcr.io/paul-henry-tremblay/some-image .
-source conf.sh
 gcloud builds submit --region=us-west1 --config cloudbuild.yaml . \
 	--substitutions=_PYPI_USERNAME=$_PYPI_USERNAME,_PYPI_PASSWORD=$_PYPI_PASSWORD
