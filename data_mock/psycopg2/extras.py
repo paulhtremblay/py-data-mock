07cd1877938b4862ee0f4422f95be6010b17d0d2
Paul Tremblay <phtremblay@sbgtv.com>
2023-06-03 11:45:58 -0700

    making dict

--- data_mock/psycopg2/extras.py
+++ data_mock/psycopg2/extras.py
@@ -2,4 +2,19 @@ _cursor = None
 class DictCursorBase():
     pass
 class RealDictCursor(DictCursorBase):
+
+    #dummy
+    def row_factory(self, *args, **kwargs):
+        pass
+
+    #dummy
+    def fetchmany(self):
+        pass
+
+    #dummy
+    def fetchall(self):
+        pass
+
+class RealDictRow:
     pass
+
--- unittests/test_psycopg2.py
+++ unittests/test_psycopg2.py
@@ -1,6 +1,7 @@
 import sys
 
-from collections.abc import Iterable 
+from collections.abc import Iterable
+from collections import OrderedDict
 
 sys.path.append('.')
 import unittest
@@ -23,43 +24,52 @@ class TestResults(unittest.TestCase):
         c = connection.cursor()
 
     def test_fetch_with_one_row(self):
-        connection = data_mock.psycopg2.connect( user='x', password='x', 
-            host='x', port='x', database='x')
-        cursor = connection.cursor()
         mock_data = [
         [('field1', 'value1'), ('field2', 'value2')],
             ]
-        cursor.execute(query = 'some query', mock_data = mock_data)
+        connection = data_mock.psycopg2.connect( user='x', password='x', 
+            host='x', port='x', database='x', mock_data = mock_data)
+        cursor = connection.cursor()
+        cursor.execute(query = 'some query')
+        number_rows = 0
         while True:
             rows = cursor.fetchmany(2000)
             if not rows:
                 break
             else:
                 for i in rows:
-                    pass
+                    number_rows += 1
+        self.assertEqual(number_rows, 1)
 
     def test_fetch_with_two_row(self):
-        connection = data_mock.psycopg2.connect( user='x', password='x', 
-            host='x', port='x', database='x')
-        cursor = connection.cursor()
         mock_data = [
         [('field1', 'value1'), ('field2', 'value2')],
         [('field3', 'value3'), ('field4', 'value4')],
             ]
-        cursor.execute(query = 'some query', mock_data = mock_data)
+        connection = data_mock.psycopg2.connect( user='x', password='x', 
+            host='x', port='x', database='x', mock_data = mock_data)
+
+        cursor = connection.cursor()
+        cursor.execute(query = 'some query')
+        number_rows = 0
         while True:
             rows = cursor.fetchmany(1)
             if not rows:
                 break
             else:
                 for i in rows:
-                    print(i)
-                    pass
+                    self.assertTrue(isinstance(i, tuple))
+                    number_rows += 1
+        self.assertEqual(number_rows, 2)
 
     def test_dict_cursor(self):
+        mock_data = [
+        [('field1', 'value1'), ('field2', 'value2')],
+            ]
         sql = "some query"
         connection = data_mock.psycopg2.connect( user='x', password='x', 
-            host='x', port='x', database='x')
+            host='x', port='x', database='x', mock_data = mock_data)
+        number_rows = 0
         with connection.cursor(
                 cursor_factory = data_mock.psycopg2.extras.RealDictCursor
                 ) as cursor:
@@ -69,7 +79,9 @@ class TestResults(unittest.TestCase):
                 if not rows:
                     break
                 for row in rows:
-                    break
+                    number_rows += 1
+                    self.assertTrue(isinstance(row, OrderedDict))
+        self.assertEqual(number_rows, 1)
 
 if __name__ == '__main__':
     unittest.main()
